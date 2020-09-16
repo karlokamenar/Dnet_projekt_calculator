45820a52d6f8cd8d982e22e00c68cdea15b3c71c
Karlo Kamenar <kakamena@vsite.hr>
2020-09-16 16:31:59 +0200

    Add project files.

---
+++ Dnet_projekt_calculator/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Dnet_projekt_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.broj7 = new System.Windows.Forms.Button();
+            this.broj8 = new System.Windows.Forms.Button();
+            this.broj9 = new System.Windows.Forms.Button();
+            this.broj4 = new System.Windows.Forms.Button();
+            this.broj5 = new System.Windows.Forms.Button();
+            this.broj6 = new System.Windows.Forms.Button();
+            this.broj1 = new System.Windows.Forms.Button();
+            this.broj2 = new System.Windows.Forms.Button();
+            this.broj3 = new System.Windows.Forms.Button();
+            this.tipkaDijeli = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.tipkaClear = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.tipkaBrisi = new System.Windows.Forms.Button();
+            this.tipkaMnozi = new System.Windows.Forms.Button();
+            this.tipkaMinus = new System.Windows.Forms.Button();
+            this.tipkaJednako = new System.Windows.Forms.Button();
+            this.tipkaPLus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tipkaKvadrat = new System.Windows.Forms.Button();
+            this.tipkaKorijen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // broj7
+            // 
+            this.broj7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj7.Location = new System.Drawing.Point(12, 118);
+            this.broj7.Name = "broj7";
+            this.broj7.Size = new System.Drawing.Size(45, 45);
+            this.broj7.TabIndex = 0;
+            this.broj7.Text = "7";
+            this.broj7.UseVisualStyleBackColor = true;
+            this.broj7.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj8
+            // 
+            this.broj8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj8.Location = new System.Drawing.Point(63, 118);
+            this.broj8.Name = "broj8";
+            this.broj8.Size = new System.Drawing.Size(45, 45);
+            this.broj8.TabIndex = 1;
+            this.broj8.Text = "8";
+            this.broj8.UseVisualStyleBackColor = true;
+            this.broj8.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj9
+            // 
+            this.broj9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj9.Location = new System.Drawing.Point(114, 118);
+            this.broj9.Name = "broj9";
+            this.broj9.Size = new System.Drawing.Size(45, 45);
+            this.broj9.TabIndex = 2;
+            this.broj9.Text = "9";
+            this.broj9.UseVisualStyleBackColor = true;
+            this.broj9.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj4
+            // 
+            this.broj4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj4.Location = new System.Drawing.Point(12, 169);
+            this.broj4.Name = "broj4";
+            this.broj4.Size = new System.Drawing.Size(45, 45);
+            this.broj4.TabIndex = 3;
+            this.broj4.Text = "4";
+            this.broj4.UseVisualStyleBackColor = true;
+            this.broj4.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj5
+            // 
+            this.broj5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj5.Location = new System.Drawing.Point(63, 169);
+            this.broj5.Name = "broj5";
+            this.broj5.Size = new System.Drawing.Size(45, 45);
+            this.broj5.TabIndex = 4;
+            this.broj5.Text = "5";
+            this.broj5.UseVisualStyleBackColor = true;
+            this.broj5.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj6
+            // 
+            this.broj6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj6.Location = new System.Drawing.Point(114, 169);
+            this.broj6.Name = "broj6";
+            this.broj6.Size = new System.Drawing.Size(45, 45);
+            this.broj6.TabIndex = 5;
+            this.broj6.Text = "6";
+            this.broj6.UseVisualStyleBackColor = true;
+            this.broj6.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj1
+            // 
+            this.broj1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj1.Location = new System.Drawing.Point(12, 220);
+            this.broj1.Name = "broj1";
+            this.broj1.Size = new System.Drawing.Size(45, 45);
+            this.broj1.TabIndex = 6;
+            this.broj1.Text = "1";
+            this.broj1.UseVisualStyleBackColor = true;
+            this.broj1.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj2
+            // 
+            this.broj2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj2.Location = new System.Drawing.Point(63, 220);
+            this.broj2.Name = "broj2";
+            this.broj2.Size = new System.Drawing.Size(45, 45);
+            this.broj2.TabIndex = 7;
+            this.broj2.Text = "2";
+            this.broj2.UseVisualStyleBackColor = true;
+            this.broj2.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // broj3
+            // 
+            this.broj3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broj3.Location = new System.Drawing.Point(114, 220);
+            this.broj3.Name = "broj3";
+            this.broj3.Size = new System.Drawing.Size(45, 45);
+            this.broj3.TabIndex = 8;
+            this.broj3.Text = "3";
+            this.broj3.UseVisualStyleBackColor = true;
+            this.broj3.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // tipkaDijeli
+            // 
+            this.tipkaDijeli.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaDijeli.Location = new System.Drawing.Point(165, 118);
+            this.tipkaDijeli.Name = "tipkaDijeli";
+            this.tipkaDijeli.Size = new System.Drawing.Size(45, 45);
+            this.tipkaDijeli.TabIndex = 9;
+            this.tipkaDijeli.Text = "/";
+            this.tipkaDijeli.UseVisualStyleBackColor = true;
+            this.tipkaDijeli.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(12, 271);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(96, 45);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "0";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // tipkaClear
+            // 
+            this.tipkaClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaClear.Location = new System.Drawing.Point(216, 67);
+            this.tipkaClear.Name = "tipkaClear";
+            this.tipkaClear.Size = new System.Drawing.Size(45, 96);
+            this.tipkaClear.TabIndex = 13;
+            this.tipkaClear.Text = "CE";
+            this.tipkaClear.UseVisualStyleBackColor = true;
+            this.tipkaClear.Click += new System.EventHandler(this.tipkaClear_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(114, 271);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(45, 45);
+            this.button14.TabIndex = 12;
+            this.button14.Text = ",";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.broj_Click);
+            // 
+            // tipkaBrisi
+            // 
+            this.tipkaBrisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaBrisi.Location = new System.Drawing.Point(216, 169);
+            this.tipkaBrisi.Name = "tipkaBrisi";
+            this.tipkaBrisi.Size = new System.Drawing.Size(45, 45);
+            this.tipkaBrisi.TabIndex = 15;
+            this.tipkaBrisi.Text = "C";
+            this.tipkaBrisi.UseVisualStyleBackColor = true;
+            this.tipkaBrisi.Click += new System.EventHandler(this.tipkaBrisi_Click);
+            // 
+            // tipkaMnozi
+            // 
+            this.tipkaMnozi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaMnozi.Location = new System.Drawing.Point(165, 169);
+            this.tipkaMnozi.Name = "tipkaMnozi";
+            this.tipkaMnozi.Size = new System.Drawing.Size(45, 45);
+            this.tipkaMnozi.TabIndex = 14;
+            this.tipkaMnozi.Text = "*";
+            this.tipkaMnozi.UseVisualStyleBackColor = true;
+            this.tipkaMnozi.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // tipkaMinus
+            // 
+            this.tipkaMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaMinus.Location = new System.Drawing.Point(165, 220);
+            this.tipkaMinus.Name = "tipkaMinus";
+            this.tipkaMinus.Size = new System.Drawing.Size(45, 45);
+            this.tipkaMinus.TabIndex = 16;
+            this.tipkaMinus.Text = "-";
+            this.tipkaMinus.UseVisualStyleBackColor = true;
+            this.tipkaMinus.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // tipkaJednako
+            // 
+            this.tipkaJednako.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaJednako.Location = new System.Drawing.Point(216, 220);
+            this.tipkaJednako.Name = "tipkaJednako";
+            this.tipkaJednako.Size = new System.Drawing.Size(45, 96);
+            this.tipkaJednako.TabIndex = 19;
+            this.tipkaJednako.Text = "=";
+            this.tipkaJednako.UseVisualStyleBackColor = true;
+            this.tipkaJednako.Click += new System.EventHandler(this.tipkaJednako_Click);
+            // 
+            // tipkaPLus
+            // 
+            this.tipkaPLus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaPLus.Location = new System.Drawing.Point(165, 271);
+            this.tipkaPLus.Name = "tipkaPLus";
+            this.tipkaPLus.Size = new System.Drawing.Size(45, 45);
+            this.tipkaPLus.TabIndex = 18;
+            this.tipkaPLus.Text = "+";
+            this.tipkaPLus.UseVisualStyleBackColor = true;
+            this.tipkaPLus.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 29);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tipkaKvadrat
+            // 
+            this.tipkaKvadrat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaKvadrat.Location = new System.Drawing.Point(12, 67);
+            this.tipkaKvadrat.Name = "tipkaKvadrat";
+            this.tipkaKvadrat.Size = new System.Drawing.Size(96, 45);
+            this.tipkaKvadrat.TabIndex = 22;
+            this.tipkaKvadrat.Text = "x^2";
+            this.tipkaKvadrat.UseVisualStyleBackColor = true;
+            this.tipkaKvadrat.Click += new System.EventHandler(this.tipkaKvadartClick);
+            // 
+            // tipkaKorijen
+            // 
+            this.tipkaKorijen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipkaKorijen.Location = new System.Drawing.Point(114, 67);
+            this.tipkaKorijen.Name = "tipkaKorijen";
+            this.tipkaKorijen.Size = new System.Drawing.Size(96, 45);
+            this.tipkaKorijen.TabIndex = 23;
+            this.tipkaKorijen.Text = "√";
+            this.tipkaKorijen.UseVisualStyleBackColor = true;
+            this.tipkaKorijen.Click += new System.EventHandler(this.tipkaKorijenClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 328);
+            this.Controls.Add(this.tipkaKorijen);
+            this.Controls.Add(this.tipkaKvadrat);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tipkaJednako);
+            this.Controls.Add(this.tipkaPLus);
+            this.Controls.Add(this.tipkaMinus);
+            this.Controls.Add(this.tipkaBrisi);
+            this.Controls.Add(this.tipkaMnozi);
+            this.Controls.Add(this.tipkaClear);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.tipkaDijeli);
+            this.Controls.Add(this.broj3);
+            this.Controls.Add(this.broj2);
+            this.Controls.Add(this.broj1);
+            this.Controls.Add(this.broj6);
+            this.Controls.Add(this.broj5);
+            this.Controls.Add(this.broj4);
+            this.Controls.Add(this.broj9);
+            this.Controls.Add(this.broj8);
+            this.Controls.Add(this.broj7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button broj7;
+        private System.Windows.Forms.Button broj8;
+        private System.Windows.Forms.Button broj9;
+        private System.Windows.Forms.Button broj4;
+        private System.Windows.Forms.Button broj5;
+        private System.Windows.Forms.Button broj6;
+        private System.Windows.Forms.Button broj1;
+        private System.Windows.Forms.Button broj2;
+        private System.Windows.Forms.Button broj3;
+        private System.Windows.Forms.Button tipkaDijeli;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button tipkaClear;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button tipkaBrisi;
+        private System.Windows.Forms.Button tipkaMnozi;
+        private System.Windows.Forms.Button tipkaMinus;
+        private System.Windows.Forms.Button tipkaJednako;
+        private System.Windows.Forms.Button tipkaPLus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button tipkaKvadrat;
+        private System.Windows.Forms.Button tipkaKorijen;
+    }
+}
+
---
+++ Dnet_projekt_calculator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dnet_projekt_calculator
+{
+    public partial class Form1 : Form
+    {
+        Double result_vrijednost = 0;
+        String operacija_napravljena = "";
+        bool operacijaJeNapravljena = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void broj_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "0" || operacijaJeNapravljena == true)
+            {
+                textBox1.Clear();
+            }
+            operacijaJeNapravljena = false;
+
+            Button button = (Button)sender;
+            if(button.Text == ",")
+            {
+                if (!textBox1.Text.Contains(","))
+                    textBox1.Text = textBox1.Text + button.Text;
+            }
+            else
+                textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (result_vrijednost != 0)
+            {
+                tipkaJednako.PerformClick();
+                operacija_napravljena = button.Text;
+                label1.Text = result_vrijednost + " " + operacija_napravljena;
+                operacijaJeNapravljena = true;
+            }
+            else
+            {
+                operacija_napravljena = button.Text;
+                result_vrijednost = Double.Parse(textBox1.Text);
+                label1.Text = result_vrijednost + " " + operacija_napravljena;
+                operacijaJeNapravljena = true;
+            }
+
+
+        }
+
+        private void tipkaClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void tipkaBrisi_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            result_vrijednost = 0;
+        }
+
+        private void tipkaJednako_Click(object sender, EventArgs e)
+        {
+            switch (operacija_napravljena)
+            {
+                case "+":
+                    textBox1.Text = (result_vrijednost + Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (result_vrijednost - Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "*":
+                    textBox1.Text = (result_vrijednost * Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    textBox1.Text = (result_vrijednost / Double.Parse(textBox1.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            result_vrijednost = Double.Parse(textBox1.Text);
+            label1.Text = "";
+        }
+
+        private void tipkaKorijenClick(object sender, EventArgs e)
+        {
+            result_vrijednost = 0;
+            operacija_napravljena = "";
+            operacijaJeNapravljena = false;
+            textBox1.Text = Math.Sqrt(Double.Parse(textBox1.Text)).ToString();
+            result_vrijednost = Double.Parse(textBox1.Text);
+            label1.Text = "";
+
+        }
+
+        private void tipkaKvadartClick(object sender, EventArgs e)
+        {
+            result_vrijednost = 0;
+            operacija_napravljena = "";
+            operacijaJeNapravljena = false;
+            textBox1.Text = (Double.Parse(textBox1.Text) * Double.Parse(textBox1.Text)).ToString();
+            result_vrijednost = Double.Parse(textBox1.Text);
+            label1.Text = "";
+
+        }
+    }
+}
